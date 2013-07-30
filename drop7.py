6ccc3d0d939255f8a7384a61fb083993932e2f00
Adam Naber <casual.formality@gmail.com>
2013-07-29 21:48:41 -0700

    Recursive scooting and Scorekeeping is now supported

--- drop7.py
+++ drop7.py
@@ -5,6 +5,7 @@ from random import randint
 ##### Global variables #####
 new_block = 0
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 WIDGET_WIDTH = WIDGET_HEIGHT = 32
@@ -47,22 +48,25 @@ class Widget:
         if self.active == 1:
             if board.arr[self.loc_y + 1][self.loc_x] != None:
                 return False
-            self.clear(screen)
             while self.loc_y < 7 and board.arr[self.loc_y + 1][self.loc_x] == None:
+                self.clear(screen)
                 self.loc_y += 1
+                self.draw(screen)
+                pygame.time.wait(100)
+                pygame.display.flip()
             board.arr[self.loc_y][self.loc_x] = self
-            self.draw(screen)
             self.active = 0
             return True
 
     def mark(self, screen, board):
-        print "Marking: " + str(self.number)
         self.delete = 1
         self.clear(screen)
 # end Widget class
 
 class Board:
     arr = [[None for x in range(7)] for y in range(8)]
+    modifier = 1
+    score = 0
 
     def check_cell(self, x, y):
         if self.arr[y][x] == None:
@@ -97,25 +101,53 @@ class Board:
             return True
         return False
 
+    def scoot(self, screen):
+        boardChanged = False
+        for x in range(7):
+            for y in range(1, 7):
+                if self.arr[y][x] != None and self.arr[y + 1][x] == None:
+                    myWidget = self.arr[y][x]
+                    myWidget.clear(screen)
+                    myWidget.loc_y += 1
+                    self.arr[y + 1][x] = myWidget
+                    self.arr[y][x] = None
+                    myWidget.draw(screen)
+                    boardChanged = True
+        return boardChanged
+
     def clean(self):
         boardChanged = False
         for x in range(7):
             for y in range(1, 8):
                 if self.arr[y][x] != None and self.arr[y][x].delete == 1:
+                    self.score += pow(7, self.modifier) 
                     boardChanged = True
+                    self.arr[y][x].clear(screen)
                     self.arr[y][x] = None
-        #if boardChanged == True
+        return boardChanged
         
+    def printScore(self, screen):
+        text = font.render("Score: " + str(self.score), 1, WHITE)
+        textpos = text.get_rect(left=250)
+        pygame.draw.rect(screen, BLACK, textpos)
+        screen.blit(text, textpos)
 
-    def check(self):
+    def check(self, screen):
         boardChanged = False
         for x in range(7):
             for y in range(1, 8):
                 if self.check_cell(x, y) == True:
                     boardChanged = True
+        pygame.time.wait(50)
+        if self.clean() == True:
+            boardChanged = True
+        pygame.time.wait(50)
+        if self.scoot(screen) == True:
+            boardChanged = True
+
         if boardChanged == True:
-            self.clean()
-        print ""
+            self.printScore(screen)
+            self.check(screen)
 # end Board class
 
 ##### Game stuff #####
@@ -124,9 +156,10 @@ clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((640, 480), DOUBLEBUF)
 pygame.display.set_caption('Drop7')
+font = pygame.font.Font(None, 12)
 
 board = Board()
-one_widget = Widget(screen)
+active_widget = Widget(screen)
 
 while 1:
     clock.tick(30)
@@ -138,13 +171,13 @@ while 1:
         if not hasattr(event, 'unicode'):
             continue
         if event.key == K_RIGHT:
-            one_widget.right(screen, board)
+            active_widget.right(screen, board)
         elif event.key == K_LEFT:
-            one_widget.left(screen, board)
+            active_widget.left(screen, board)
         elif event.key == K_DOWN:
-            if one_widget.drop(screen, board) == True:
-                one_widget = Widget(screen)
-                board.check()
+            if active_widget.drop(screen, board) == True:
+                active_widget = Widget(screen)
+                board.check(screen)
         elif event.key == K_ESCAPE or event.key == K_q: 
             sys.exit(0)
 
