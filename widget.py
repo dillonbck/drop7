9b90d6e0af5732530708d88e6552ce8a1719a418
Dillon Beck <dillonbck@gmail.com>
2015-03-13 21:51:25 -0500

    Documentation and pylint changes
    Added Module, Class, and Method docstrings.
    Updated code to fix pylint issues.

--- widget.py
+++ widget.py
@@ -1,3 +1,11 @@
+"""
+File: widget.py
+Author: Dillon Beck
+
+Classes:
+    Widget: Manages widget attributes.
+
+"""
 import logging
 import pygame
 from random import randint
@@ -5,31 +13,43 @@ from random import randint
 import config
 
 
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
-class Widget:
-    def __init__(self, unbroken=False, x=0, y=0):
-        self.number = randint(1,7)
-        #self.number = 2
-        if unbroken or (randint(1,8) == 8):    # mostly broken
-        #if unbroken or (randint(1,8) > 9):     # all unbroken
-        #if (randint(1,8) > 2):     # mostly unbroken
+class Widget(object):
+    """Widget class that manages breaking/cracking widgets and movement.
+
+    #TODO: loc_x and loc_y -> (loc_X, loc_y) or (row, column)
+    #TODO: unbroken/cracked -> one int attribute? 
+    #TODO: sprite should be here?
+
+    Attribute:
+        number: Int representing the number of the widget.
+        unbroken: Boolean indicating whether widget is unbroken.
+        cracked: Boolean indicating whether widget is cracked.
+        sprite: Sprite for the widget.
+        loc_x: Row the widget occupies.
+        loc_y: Column the widget occupies.
+        active: Whether the widget is the one being moved by the user.
+        delete: Whether the widget is deleted or not.
+
+    """
+
+    def __init__(self, unbroken=False, row=0, column=0):
+        self.number = randint(1, 7)
+        if unbroken or (randint(1, 8) == 8):
             self.unbroken = True
             self.cracked = False
             if config.use_gui:
                 self.sprite = config.icon_arr[7]
-            #self.number = 4
         else:
             self.unbroken = False
             self.cracked = False
             if config.use_gui:
                 self.sprite = config.icon_arr[self.number - 1]
-            #color = self.sprite.get_at((0,0))
-            #self.sprite.set_colorkey(color)
 
         if unbroken:
-            self.loc_x = x
-            self.loc_y = y
+            self.loc_x = row
+            self.loc_y = column
             self.active = 0
         else:
             self.loc_x = config.DROP_X
@@ -39,32 +59,53 @@ class Widget:
         self.delete = 0
         self.draw()
 
-    def clear(self):
-        # if self.active:
-        #     box = pygame.Rect(self.loc_x * config.WIDGET_WIDTH, (self.loc_y+1) * config.WIDGET_HEIGHT, config.WIDGET_WIDTH-1, config.WIDGET_HEIGHT-1)
 
-        # else:
-        #     box = pygame.Rect(self.loc_x * config.WIDGET_WIDTH, (self.loc_y+2) * config.WIDGET_HEIGHT, config.WIDGET_WIDTH-1, config.WIDGET_HEIGHT-1)
-        # pygame.draw.rect(config.BLACK, box, 0)
-        # #pygame.draw.rect(WHITE)
+    def clear(self):
+        """Remove the widget from the screen.
+
+        #TODO: This is gui stuff...
+
+        """
 
         if config.use_gui:
             config.gui.clear_widget(self.active, self.loc_x, self.loc_y)
 
             pygame.display.flip()
 
+
     def draw(self):
-        # if self.active:
-        #     screen.blit(self.sprite, (self.loc_x * config.WIDGET_WIDTH, (self.loc_y+1) * config.WIDGET_HEIGHT))
-        # else:
-        #     screen.blit(self.sprite, (self.loc_x * config.WIDGET_WIDTH, (self.loc_y+2) * config.WIDGET_HEIGHT))
+        """Draw the widget on the screen.
+
+        #TODO: This is gui stuff..
+
+        """
+
 
         if config.use_gui:
             config.gui.draw_widget(self.sprite, self.active, self.loc_x, self.loc_y)
 
             pygame.display.flip()
 
+
     def redraw(self, prev_x=None, prev_y=None, prev_active=None):
+        """Redraw the widget on the board.
+
+        #TODO: This is all gui stuff... should it be here?
+
+        Args:
+            prev_x: The x location where the widget just was.  This is where the
+                sprite for the widget is currently drawn on the screen before
+                being updated here.  If None, the widget's last and current x
+                position are the same.
+            prev_y: The y location where the widget just was.  This is where the
+                sprite for the widget is currently drawn on the screen before
+                being updated here.  If None, the widget's last and current y
+                position are the same.
+            prev_active: Int indicating the widget's previous active state.
+            #TODO: Should self.active be an into 1 or 0, or a bool?
+
+        """
+
         if prev_x is None:
             prev_x = self.loc_x
         if prev_y is None:
@@ -76,25 +117,42 @@ class Widget:
             config.gui.clear_widget(prev_active, prev_x, prev_y)
             config.gui.draw_widget(self.sprite, self.active, self.loc_x, self.loc_y)
 
-    def right(self):
-        if self.loc_x < 6 and self.active == 1:
 
-            #self.clear()
+    def right(self):
+        """Move the widget right 1 spot.
+
+        The widget will move right 1 spot, but its x location will not be 
+        greater than 7.
+
+        """
+
+        if self.loc_x < 6 and self.active == 1:
             self.loc_x += 1
-            #self.draw()
+
             self.redraw(prev_x=self.loc_x-1)
 
+
     def left(self):
+        """Move the widget left 1 spot.
+
+        The widget will move left 1 spot, but its x location will not be less
+        than 0.
+
+        """
+
         if self.loc_x >= 1 and self.active == 1:
-            #self.clear()
             self.loc_x -= 1
-            #self.draw()
 
             self.redraw(prev_x=self.loc_x+1)
     
 
     # Mark a cell to be deleted
     def remove(self):
+        """Mark a widget as deleted.
+        #TODO: Should this even be a method?
+        #TODO: Should/can this be renamed to delete?
+        """
+
         self.delete = 1
 
         if config.use_gui:
@@ -103,6 +161,15 @@ class Widget:
 
 
     def check_break(self):
+        """Break or crack the widget.
+
+        Check if the widget is unbroken or cracked.  If unbroken, crack the 
+        widget.  If cracked, break the widget.  If broken, do nothing.
+        Updates the widget's sprite.
+        #TODO: shouldn't have any stuff to do with the sprite?
+
+        """
+
         new_sprite = None
 
         if  self.cracked == True:
